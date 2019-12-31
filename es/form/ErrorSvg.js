b748961a643529b65b43f31ad78dd359deb6fc5d
season <season.su@fwd.com>
2019-12-31 12:24:51 +0800

    init antd

---
+++ es/form/ErrorSvg.js
@@ -0,0 +1,17 @@
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+import React from 'react';
+/* eslint-disable */
+
+var SvgComponent = function SvgComponent(props) {
+  return React.createElement("svg", _extends({
+    viewBox: "0 0 205.9 200"
+  }, props), React.createElement("path", {
+    d: "M148.8 128.2c1.5 1.5 1.5 4 0 5.5l-12.2 12.2c-1.5 1.5-4 1.5-5.5 0l-28.2-28.2-28.2 28.2c-1.5 1.5-4 1.5-5.5 0L57 133.7c-1.5-1.5-1.5-4 0-5.5L85.3 100 57.1 71.8c-1.5-1.5-1.5-4 0-5.5l12.2-12.2c1.5-1.5 4-1.5 5.5 0L103 82.3l28.2-28.2c1.5-1.5 4-1.5 5.5 0l12.2 12.2c1.5 1.5 1.5 4 0 5.5L120.6 100l28.2 28.2z"
+  }));
+};
+/* eslint-enable */
+
+
+export default SvgComponent;
+//# sourceMappingURL=ErrorSvg.js.map
